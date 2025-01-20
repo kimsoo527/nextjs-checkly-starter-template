88cc3d1e27f3a8d89a48de7f3abe096b414c2dac
tnolet <tim@checklyhq.com>
2025-01-20 16:12:19 +0100

    feat: update and parametrize some urls

--- __checks__/api.check.ts
+++ __checks__/api.check.ts
@@ -1,11 +1,23 @@
-import { ApiCheck, Frequency } from 'checkly/constructs'
+import { ApiCheck, Frequency, AssertionBuilder, RetryStrategyBuilder } from 'checkly/constructs'
 
 new ApiCheck('api-check-1', {
   name: 'Greetings API',
   frequency: Frequency.EVERY_1H,
   tags: ['api'],
-  request: {
-    url: 'https://nextjs-vercel.app/api/greetings',
-    method: 'GET',
+  setupScript: {
+    content: "process.env.BASE_URL = process.env.ENVIRONMENT_URL || 'https://nextjs-checkly-starter-template.vercel.app'",
   },
+  request: {
+    url: '{{BASE_URL}}/api/greetings',
+    method: 'GET',
+    assertions: [
+      AssertionBuilder.statusCode().equals(200),
+      AssertionBuilder.jsonBody('$[0].text').notEmpty()
+    ]
+  },
+  retryStrategy: RetryStrategyBuilder.exponentialStrategy({
+    maxRetries: 3,
+    baseBackoffSeconds: 1,
+    sameRegion: true
+  })
 })
--- __checks__/landing-page.spec.ts
+++ __checks__/landing-page.spec.ts
@@ -1,7 +1,9 @@
 import { test, expect } from '@playwright/test'
 
 test('Landing page', async ({ page }) => {
-  const targetUrl = process.env.ENVIRONMENT_URL || 'https://otel-test-app.vercel.app'
-  const response = await page.goto(targetUrl)
+  const baseURL = process.env.ENVIRONMENT_URL || 'https://nextjs-checkly-starter-template.vercel.app'
+  const response = await page.goto(baseURL)
   expect(response?.status()).toBeLessThan(400)
+  await page.screenshot({ path: 'test-results/screenshot/landing-page.jpg' })
+  await expect(page.getByText('deploy now')).toBeVisible()
 })
